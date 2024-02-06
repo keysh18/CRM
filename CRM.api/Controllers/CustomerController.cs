7a14f00f8ffeaa1e674cf4354deff0c6162bf1ed
keysh18 <114112736+keysh18@users.noreply.github.com>
2024-02-06 12:06:18 -0600

    Create Customer Controller

---
+++ CRM.api/Controllers/CustomerController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CRM.api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class CustomerController: ControllerBase
+    {
+        [HttpGet]
+        [Route("all")]
+        public async Task<IActionResult> GetAllCustomersAsync()
+        {
+            return Ok();
+        }
+
+
+    }
+
+}
