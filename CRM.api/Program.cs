883564063ac3abd01a9537f27958f49d36063bfa
OumarouWatS <Salome.oumarou@gmail.com>
2024-03-05 10:33:20 -0600

    Adding DB context.

--- CRM.api/Program.cs
+++ CRM.api/Program.cs
@@ -3,6 +3,13 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<WLSSDbContext>(opt =>
+    opt.UseSqlServer(
+            builder.Configuration.GetConnectionString("WLSSCoreDatabase"),
+            config => config.EnableRetryOnFailure()
+        )
+);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
